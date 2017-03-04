f0c5e643429395480e56b50da799cdd5dd174025
Hannah Vivian Shaw <hvivianshaw@gmail.com>
2017-03-04 00:17:37 +0000

    Needs moar semicolons

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 app.use(express.static('static'));
-app.set('views', 'templates')
-app.set('view engine', 'pug')
+app.set('views', 'templates');
+app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
-  res.render('index', {basedir: __dirname})
-})
+  res.render('index', {basedir: __dirname});
+});
 
 app.listen(process.env.PORT || 8080, function () {
-  console.log('API is listening!')
-})
+  console.log('API is listening!');
+});
